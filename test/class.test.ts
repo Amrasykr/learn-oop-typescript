ae0433c053e151657a54123e4d0b22193b08af48
Ammar Asysyakur <asysyakurammar125@gmail.com>
2025-01-28 22:20:30 +0700

    feat: constructor

--- test/class.test.ts
+++ test/class.test.ts
@@ -1,15 +1,21 @@
 describe('Class', () => {
+    class Customer {
+        constructor() {
+            console.info("new customer created")
+        }
+    }
+
+    class Order {
+
+    }
+
     it('should can create class', () => {
-
-        class Customer {
-
-        }
-
-        class Order {
-
-        }
-
         const customer: Customer = new Customer()
         const order: Order = new Order()
+    });
+
+    it('should can create constructor', () => {
+        new Customer()
+        new Customer()
     });
 })
