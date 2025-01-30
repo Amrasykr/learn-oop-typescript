13d3fc2eb406706707b26b98f35b72c872ef7aeb
Ammar Asysyakur <asysyakurammar125@gmail.com>
2025-01-30 11:02:43 +0700

    feat: visibility

---
+++ test/visibility.test.ts
@@ -0,0 +1,40 @@
+describe('Visibility', () => {
+
+    class Counter {
+
+        protected counter: number = 0
+
+        public increment(): void{
+            this.counter++
+        }
+
+        public getCounter(): number {
+            return this.counter;
+        }
+    }
+
+    class DoubleCounter extends Counter {
+
+        public increment() {
+            this.counter += 2
+        }
+
+    }
+    it('should support ', () => {
+
+        const counter: Counter = new Counter()
+        counter.increment()
+        counter.increment()
+        console.info(counter.getCounter())
+    });
+
+
+    it('should support protected', () => {
+
+        const counter: DoubleCounter = new DoubleCounter()
+        counter.increment()
+        counter.increment()
+        console.info(counter.getCounter())
+
+    });
+});
